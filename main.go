f44cadc027e88b80d78e043232824bde3f93d5a1
borellif <fborelli.alt@gmail.com>
2022-05-01 22:36:03 -0400

    Renames math to main

--- main.go
+++ main.go
@@ -8,6 +8,8 @@ import (
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/csrf"
 	"github.com/gofiber/template/html"
+
+	"github.com/borellif/Math-Web-Api/math"
 )
 
 func main() {
@@ -32,21 +34,14 @@ func main() {
 		})
 	})
 
-	app.Get("/min", func(c *fiber.Ctx) error {
-		c.Request()
-	})
-
-	// app.Get("/max", func(c *fiber.Ctx) error {
-	// })
-
-	// app.Get("/avg", func(c *fiber.Ctx) error {
-	// })
-
-	// app.Get("/median", func(c *fiber.Ctx) error {
-	// })
-
-	// app.Get("/percentile", func(c *fiber.Ctx) error {
-	// })
-
+	setupRoutes(app)
 	app.Listen(":3000")
+}
+
+func setupRoutes(app *fiber.App) {
+	app.Get("/api/v1/min", math.Minimum)
+	app.Get("/api/v1/max", math.Maximum)
+	app.Get("/api/v1/avg", math.Average)
+	app.Get("/api/v1/median", math.Median)
+	app.Get("/api/v1/percentile", math.Percentile)
 }
